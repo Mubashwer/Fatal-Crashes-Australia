24023e969fb862304bce78e60f1783fd05fc3aab
Mubashwer Salman Khurshid <mubashwersk@gmail.com>
2014-05-13 22:34:41 +1000

    Update insights.py

--- insights.py
+++ insights.py
@@ -34,68 +34,117 @@ html_end = '''
 csv_loc = "fatal_crashes_5_years.csv"
 
 # hypothesis titles ....[PHILIP, EDIT THEM AFTER ALL GRAPHS HAS BEEN GENERATED]
-ht_header1 = 'The number of fatalities due to road crashes has decreased in\
-              the last 5 years.'
-ht_header2 = 'There has been more fatalities due to road crashes between\
-              1pm-6pm than any other 5-hour interval in the last 5 years.'
-ht_header3 = 'There has been more fatalities due to road crashes in NSW than\
-              any other state in the last 5 years.'
-ht_header4 = 'There has been more fatalities due to road crashes in Friday\
-              and the weekends than the other weekdays.'
-ht_header5 = 'Speed Limit lower than 50 has very few fatalities.'
+ht_header1 = '''The total number of fatalities due to road crashes decreased in the last 5 years.'''
+ht_header2 = '''There were more fatalities due to road crashes towards the end of the week.'''
+ht_header3 = '''There were more fatalities due to road crashes between 1pm-6pm than any other 5-hour interval.'''
+ht_header4 = '''There were more fatalities due to road crashes in New South Wales than any other state.'''
+ht_header5 = '''There were very few fatalities due to crashes in roads with speed limit lower than 50 km/h.'''
 
 # hypothesis explanations [PHILIP, WRITE THESE PARAGRAPHS AFTER ALL GRAPHS HAS BEEN GENERATED]
-ht_xpln1 = ''
-ht_xpln2 = ''
-ht_xpln3 = ''
-ht_xpln4 = ''
+ht_xpln1 = '''efwsegwsegeswrtfwegws'''
+ht_xpln2 = '''qrfqt3eqftq3et3qt'''
+ht_xpln3 = '''etqetrw3trwt4t4t4t4t'''
+ht_xpln4 = '''It can be seen in the bar chart that the number of fatalities in New South Wales is larger than all
+ other stated by a great margin. One of the reasons is that it is the most populated state. States with relatively low
+ population had very few fatalities such as Northern Territory, Australian Capital Territory and Tasmania.'''
+ht_xpln5 = '''q3erfefrewqr343r3rer'''
 
 # independent variable for data dictionaries for each hypothesis
 ht_var1 = 'Year'
-ht_var2 = 'Hour'
-ht_var3 = 'State'
-ht_var4 = 'Dayweek'
+ht_var2 = 'Dayweek'
+ht_var3 = 'Hour'
+ht_var4 = 'State'
 ht_var5 = 'Speed Limit'
 
 dep_var = 'Number of Fatalities' # depedendent variable
 
+# file names for each visualisation
+ht_img1 = 'vs_year.png'
+ht_img2 = 'vs_day.png'
+ht_img3 = 'vs_hour.png'
+ht_img4 = 'vs_state.png'
+ht_img5 = 'vs_speed_limit.png'
+
+page ='''
+<h1>When and where in Australia have fatal crashes occured the most in the last 5 years?</h1>
+<div class="hypothesis" id="hypothesis1">
+    <h2 class="header">{}</h2>
+    <div class="visual"><img src="{}" alt="image1" width="500" height="375"></div>
+    <p class="explain">{}</p>
+</div>
+<div class="hypothesis">
+    <h2 class="header" id="header2">{}</h2>
+    <div class="visual"><img src="{}" alt="image2" width="500" height="375"></div>
+    <p class="explain">{}</p>
+</div>
+<div class="hypothesis" id="hypothesis3">
+    <h2 class="header">{}</h2>
+    <div class="visual"><img src="{}" alt="image2" width="500" height="375"></div>
+    <p class="explain">{}</p>
+</div>
+<div class="hypothesis">
+    <h2 class="header">{}</h2>
+    <div class="visual""><img src="{}" alt="image2" width="500" height="375"></div>
+    <p class="explain">{}</p>
+</div>
+<div class="hypothesis">
+    <h2 class="header">{}</h2>
+    <div class="visual"><img src="{}" alt="image2" width="500" height="375"></div>
+    <p class="explain"">{}</p>
+</div>
+'''.format(ht_header1, ht_img1, ht_xpln1, ht_header2, ht_img2, ht_xpln2, ht_header3, ht_img3, ht_xpln3, ht_header4, ht_img4, ht_xpln4, ht_header5, ht_img5, ht_xpln5)
+
+
+
+
 
 
 def webshow(img): # call this function and serve to check graph
-    savefig(img, dpi=50)
-    print '<img width="400" height="300" src="'+img+'" />'
+    savefig(img, dpi=80)
+    #print '<img width="400" height="300" src="'+img+'" />'
 
 
 
 def vs_year(data): #ASIR, line graph
     clf()
-    print data # test
+    y = [item[1] for item in sorted(data.items(), key=lambda x: x[0])]
+    plot(y, color='purple')
+    xticks(arange(5), sorted(data.keys()), rotation=30)
+    grid(True)
+    xlabel('Time (Year)')
+    ylabel(dep_var)
+    title('Line Graph for Fatalities vs Time')
+    webshow(ht_img1)
 
-
-
-def vs_hour(data): #MUBASHWER, histogram
-    clf()
-    hist(data.keys(), bins = arange(0,25), weights = data.values(), facecolor="green")
-    xticks(arange(0,24,2), ['{:02d}'.format(hour) for hour in arange(0,24,2)])
-    grid(True) 
-    xlabel('Time (Hours in a Day)')
-    ylabel('Number of fatalities')
-    title('Histogram for Fatalities vs Time')
-    webshow('vs_hour.png')
     
-
-
-
-def vs_state(data): #ASIR, bar chart
-    clf()
-    print data # test
-
-
-
 def vs_day(data): #PHILIP, Pie Chart
     clf()
     print data # test
 
+
+def vs_hour(data): #MUBASHWER, histogram
+    clf()
+    hist(data.keys(), bins = arange(0,25), weights = data.values(), facecolor='green')
+    xticks(arange(0,24,2), ['{:02d}'.format(hour) for hour in arange(0,24,2)])
+    grid(True) 
+    xlabel('Time (Hours in a Day)')
+    ylabel(dep_var)
+    title('Histogram for Fatalities vs Time')
+    webshow(ht_img3)
+    
+    
+def vs_state(data): #ASIR, bar chart
+    clf()
+    bars = arange(len(data))
+    heights = [item[1] for item in sorted(data.items(), key=lambda x: x[0])]
+    bar(bars, heights, align='center', facecolor='orange')
+    xticks(bars, sorted(data.keys()), rotation=30)
+    grid(True) 
+    xlabel('State')
+    ylabel(dep_var)
+    title('Bar Chart for Fatalities vs State')    
+    webshow(ht_img4)   
+    
 
 
 def vs_speed_limit(data): #MUBASHWER, Scatter Plot
@@ -104,9 +153,9 @@ def vs_speed_limit(data): #MUBASHWER, Scatter Plot
     yticks(arange(0,3000,500))
     grid(True) 
     xlabel('Speed Limit (km/h)')
-    ylabel('Number of Fatalities')
+    ylabel(dep_var)
     title('Scatter Plot for Fatalities vs Speed Limit')
-    webshow('vs_speed_limit.png')
+    webshow(ht_img5)
     
     
 # main function which controls all the action                    
@@ -124,9 +173,9 @@ def main():
     for row in data:
         fatalities = int(row[dep_var])
         ht_data1[row[ht_var1]] += fatalities
-        if '-9' not in row[ht_var2]:
-            ht_data2[int(row[ht_var2])] += fatalities
-        ht_data3[row[ht_var3]] += fatalities
+        ht_data2[row[ht_var2]] += fatalities
+        if '-9' not in row[ht_var3]:
+            ht_data3[int(row[ht_var3])] += fatalities
         ht_data4[row[ht_var4]] += fatalities
         if '-9' not in row[ht_var5]:
             ht_data5[int(row[ht_var5])] += fatalities
@@ -134,14 +183,15 @@ def main():
     print 'Content-Type: text/html\n'        
     
     # remove the hashtag for the function you are working on to test them
-    #vs_year(ht_data1)
-    vs_hour(ht_data2)
-    #vs_state(ht_data3)
-    #vs_day(ht_data4)
-    #vs_speed_limit(ht_data5)
+    vs_year(ht_data1)
+    #vs_day(ht_data2)
+    vs_hour(ht_data3)
+    vs_state(ht_data4)
+    vs_speed_limit(ht_data5)
     
+    print html_start
+    print page
+    print html_end
         
 # main function which controls all the action
 main()
-
-
